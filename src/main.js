02c1db1c88f92331f01aab9f8f5458ba92641e6a
Andrei Kucherov <andrei@kucherov.net>
2019-01-11 17:06:20 +1100

    "Edit" menu is added

--- src/main.js
+++ src/main.js
@@ -6,11 +6,33 @@ const Bot = require("./bots/binance-bot");
 
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
+const Menu = electron.Menu
 const ipc = electron.ipcMain
 
 let mainWindow
 
 const bot = new Bot().options({ quoteAsset: "USDT" });
+
+const template = [{
+    label: "Application",
+    submenu: [
+        { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+        { type: "separator" },
+        { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }}
+    ]}, {
+    label: "Edit",
+    submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+    ]}
+];
+
+Menu.setApplicationMenu(Menu.buildFromTemplate(template));
 
 app.on('ready', _ => {
     mainWindow = new BrowserWindow(config.getWindowSize())
